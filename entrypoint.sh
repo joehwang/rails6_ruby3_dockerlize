4c4ef803f765d86e0b6ca467f8c915c71f2e675e
joehwang <joehwang.com@gmail.com>
2021-03-24 18:29:31 +0800

    edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# YOUR_JUST_ONCE_LOGIC_HERE
+if [ ! -e "Gemfile" ]; then
+  bundle init && \
+  echo "gem 'rails', '~> 6.0', '>= 6.1.3'" >> Gemfile 
+fi
+
 CONTAINER_ALREADY_STARTED="config.ru"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then  
   echo "-- First container startup --"
-  # YOUR_JUST_ONCE_LOGIC_HERE
-  bundle init && \
-  echo "gem 'rails', '~> 6.0', '>= 6.1.3'" >> Gemfile && \
   bundle install && \
   rails new . --force --database=mysql --skip-bundle --skip-webpack-install && \
   bundle install && \
